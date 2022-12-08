c813b18c1a4fd810344017a2c5a7e45c5527d214
Sam McLeod <sammcj@users.noreply.github.com>
2022-12-08 17:16:29 +1100

    feat: improved ncc, refactor code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const { getInput, setOutput, setFailed } = require('@actions/core');
+const core = require('@actions/core');
 const github = require('@actions/github');
 const DEFAULT_COMMENT_IDENTIFIER = '4YE2JbpAewMX4rxmRnWyoSXoAfaiZH19QDB2IR3OSJTxmjSu';
-const ctx = github.context;
 
 async function checkForExistingComment(octokit, repo, owner, issue_number, commentIdentifier) {
   const existingComments = await octokit.issues.listComments({
@@ -21,21 +20,22 @@ async function checkForExistingComment(octokit, repo, owner, issue_number, comme
 
 async function run() {
   try {
-    const commentMessage = getInput('message');
-    const commentId = getInput('COMMENT_IDENTIFIER')
-      ? getInput('COMMENT_IDENTIFIER')
+    const ctx = github.context;
+    const commentMessage = core.getInput('message');
+    const commentId = core.getInput('COMMENT_IDENTIFIER')
+      ? core.getInput('COMMENT_IDENTIFIER')
       : DEFAULT_COMMENT_IDENTIFIER;
-    const githubToken = getInput('GITHUB_TOKEN');
+    const githubToken = core.getInput('GITHUB_TOKEN');
 
-    const issue_id = getInput('ISSUE_ID') ? getInput('ISSUE_ID') : ctx.payload.pull_request.number;
+    const issue_id = core.getInput('ISSUE_ID') ? core.getInput('ISSUE_ID') : ctx.payload.pull_request.number;
     const { owner, repo } = ctx.repo;
 
     if (!issue_id) {
-      setFailed('Action must run on a Pull Request.');
+      core.setFailed('Action must run on a Pull Request.');
       return;
     }
 
-    const octokit = github.getOctokit(githubToken);
+    const octokit = new github.GitHub(githubToken);
 
     // Suffix comment with hidden value to check for updating later.
     const commentIdSuffix = `\n\n\n<hidden purpose="for-rewritable-pr-comment-action-use" value="${commentId}"></hidden>`;
@@ -67,9 +67,9 @@ async function run() {
       });
     }
 
-    setOutput('comment-id', comment.data.id);
+    core.setOutput('comment-id', comment.data.id);
   } catch (e) {
-    setFailed(e.message);
+    core.setFailed(e.message);
   }
 }
 
