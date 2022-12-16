b4c467652f70b6e021e2b6528c3561dacaa64911
Sam McLeod <sammcj@users.noreply.github.com>
2022-12-16 11:26:24 +1100

    feat: build and publish container image

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function checkForExistingComment(octokit, issue_number, commentIdentifier,
     owner,
     repo,
     pull_number: issue_number,
-    review_id: github.context.payload.number
+    review_id: github.context.payload.number,
   });
 
   if (!existingComments.data) {
